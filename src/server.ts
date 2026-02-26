69b850eec9958862b96b86e5d188b5362832f29b
itsluisjim <jimenezluis..lj85@gmail.com>
2026-02-25 21:29:56 -0600

    add routing

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata";
+import { inject, injectable } from "tsyringe";
+import { TranscriptServiceController } from "./controllers/TranscriptControllers";
+import express from "express";
+import cors from "cors";
+
+@injectable()
+export class AppServer {
+    private server: express.Express;
+    private router: express.Router;
+    private port: number;
+    private transcriptServiceController: TranscriptServiceController;
+
+    constructor(@inject("Port") port = 9000, @inject("TranscriptServiceController") transcriptServiceController: TranscriptServiceController) {
+        this.port = port;
+        this.transcriptServiceController = transcriptServiceController;
+        this.server = express();
+        this.router = express.Router();
+    }
+
+    public start() {
+        this.server.use(cors())
+        this.server.use(express.json());
+        this.server.use(express.urlencoded({ extended: false }));
+
+        this.server.use("/api/transcript", this.router);
+        
+        this.router.get('/list', (req, res) => this.transcriptServiceController.listTranscripts(req, res));
+        this.router.get('/:id', (req: any, res: any) => this.transcriptServiceController.getTranscript(req, res, req.params.id));
+        this.router.post('/upload', (req, res) => this.transcriptServiceController.uploadTranscript(req, res));
+
+        // global error handler
+        this.server.use((err: any, req: any, res: any, next: any) => {
+            console.log("Global error:", err);
+
+            res.status(err.status || 500).json({
+                success: false,
+                message: err.message || "Internal Server Error",
+            });
+        });
+
+        this.server.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+}
